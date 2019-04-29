139adf538cc5e9dab4545871f85d09bc4a212414
lelethungalo@gmail.com <leletungalo@gmail.com>
2019-04-29 05:09:17 +0200

    done but driyer removes before add

--- src/dishWashS/WetDishRack.java
+++ src/dishWashS/WetDishRack.java
@@ -2,37 +2,36 @@ package dishWashS;
 import java.util.ArrayList;
 import java.util.concurrent.Semaphore;
 
+
 public class WetDishRack {
 	// add variables
-	private int rackSize,count;
-	Semaphore semaphore;
-	ArrayList<Integer> rack ;
+	private int rackSize;
+	private Semaphore washersemaphore;
+	private Semaphore dryerSemaphore;
+	private ArrayList<Integer> rack ;
 	WetDishRack(int rackSize) {
-	    // add correct code here
 		this.rackSize = rackSize;
-		semaphore = new Semaphore(0);
+		washersemaphore = new Semaphore(1);
+		dryerSemaphore = new Semaphore(0);
 		rack = new ArrayList<>(rackSize);
-		count = 0;
 	}
 	
 	public void addDish(int dish_id)  throws InterruptedException {
-
-		if(semaphore.availablePermits() < rackSize){
-			semaphore.release();
+		if(washersemaphore.availablePermits() >= 0){
+			washersemaphore.acquire();
 			rack.add(dish_id);
-			count++;
+			washersemaphore.release();
+			dryerSemaphore.release();
 		}
 
 	}
 	
 	public int removeDish() throws InterruptedException {
-		//if (semaphore.availablePermits() > 0){
-			semaphore.acquire();
-			int dish = rack.get(0);
-			rack.remove(0);
-			return dish;
-		//}
-		//return -1;
+
+		dryerSemaphore.acquire();
+		int dish = rack.get(0);
+		rack.remove(0);
+		return dish;
 	}
 	
 }
